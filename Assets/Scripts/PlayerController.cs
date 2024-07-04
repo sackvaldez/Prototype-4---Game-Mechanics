49a4f2f8e144ff646542ddca854873e61e1151f0
Sack Valdez <rauldaxo@gmail.com>
2024-07-03 23:56:57 -0600

    Update PlayerController.cs
    We add movement to the player in the forward direction of the focal point

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody playerRb;
+    private GameObject focalPoint;
     public float speed = 5.0f;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
+        // We get the input from the player
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        // We add movement to the player in the forward direction of the focal point
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 } 
