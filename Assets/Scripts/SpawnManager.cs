05fec25e8b5e6a77a0527fd67e3b799f0ac376ed
Sack Valdez <rauldaxo@gmail.com>
2024-07-08 20:41:25 -0600

    Random Spawning with a new method
    Now we instantiate the enemy prefab at a randomPos variable position with a Random.Range floats stored in a Vector3
    
    Also, we created a method to generate a random spawn position for the enemy, so we put everything inside

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,18 +6,28 @@ public class SpawnManager : MonoBehaviour
 {
     // We create a public GameObject variable to store the enemy prefab
     public GameObject enemyPrefab;
-
+    private float spawnRange = 9;
 
     // Start is called before the first frame update
     void Start()
     {
-        // We instantiate the enemy prefab at the position (0, 0, 0) with the rotation of the prefab
-        Instantiate(enemyPrefab, new Vector3(0, 0, 0), enemyPrefab.transform.rotation);
+        // We instantiate the enemy prefab at a randomPos variable position
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 GenerateSpawnPosition()
+    {
+        // We get a random position on the x and z axis within the spawn range
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        // We store the random position in a Vector3 variable
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
     }
 }
