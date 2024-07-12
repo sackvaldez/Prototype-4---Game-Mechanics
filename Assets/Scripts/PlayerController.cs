48b630465b6e996abfb0a2ac4249c3bb7e043362
Sack Valdez <rauldaxo@gmail.com>
2024-07-11 18:49:29 -0600

    Powerup Indicator created
    It is now possible to see a GameObject acting as a power indicator whenever the player picks it up, we turn off the indicator when the coutdown is over.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public GameObject powerupIndicator;
     public float speed = 5.0f;
     private float powerupStrength = 15.0f;
     public bool hasPowerup;
@@ -24,6 +25,8 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         // We add movement to the player in the forward direction of the focal point
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        // We move the powerup indicator to the player's position
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     // We create a method to add powerup effects to the player
@@ -32,12 +35,13 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            // With setActive we can enable or disable the powerup indicator
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             // We start the coroutine below to make the powerup last for a limited time
             StartCoroutine(PowerupCountdownRoutine());
         }
     }
-
 //    // In order to make the powerup last for a limited time, 
 //    we create a coroutine that will set hasPowerup to false after 7 seconds
     IEnumerator PowerupCountdownRoutine()
@@ -45,6 +49,7 @@ public class PlayerController : MonoBehaviour
         // Yield is a keyword that is used to return an IEnumerator, so we can use it in a coroutine
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
     
     private void OnCollisionEnter(Collision collision)
