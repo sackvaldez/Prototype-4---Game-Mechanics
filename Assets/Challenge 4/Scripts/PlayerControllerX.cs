64fd969bfb0bbb26931329e47a13305945f441b1
Sack Valdez <rauldaxo@gmail.com>
2024-07-15 23:08:00 -0600

    Challenge 4:  Football Scripts
    In this new scene, we have some problems to fix in order to be able to score goals while trying to avoid getting scored.
    
    First of all, we improved the way we collided with the enemy because he was the one kicking us instead of us kicking him.
    
    Second, the powerup was not working as intended and enemy spawning wasn´t showing the correct amount of objects.
    
    Third, the enemies didn´t move at all towards the goal, so we put some movement and now with each wave, enemy speed is increased.
    
    Fourth, there is now a boost element when pressing the spacebar key to move faster, which also includes a particle effect next to the focal Point element.

---
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public ParticleSystem boostIndicator;
+    private float speed = 500;
+    public float boostForce = 7; // force to add to player when hitting space bar
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+
+    private float normalStrength = 8; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+
+    private bool isBoostOnCooldown = false; // flag to check if boost is on cooldown
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+
+        // Check if space bar is pressed and boost is not on cooldown
+        if (Input.GetKeyDown(KeyCode.Space) && !isBoostOnCooldown)
+        {
+            boostIndicator.Play();
+            playerRb.AddForce(focalPoint.transform.forward * boostForce, ForceMode.Impulse);
+            StartCoroutine(BoostCountdownRoutine());
+        }
+    }
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // Coroutine to handle boost cooldown
+    IEnumerator BoostCountdownRoutine()
+    {
+        isBoostOnCooldown = true; // Set cooldown flag
+        yield return new WaitForSeconds(2); // Duration of the boost cooldown
+        isBoostOnCooldown = false; // Reset cooldown flag
+        boostIndicator.Stop(); // Stop boost indicator
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position; 
+           
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+        }
+    }
+}
